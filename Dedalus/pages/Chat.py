6e1e98c8229d5ddee2953892cc0b0c8c1718a753
DiegoSicre <0611146788@uma.es>
2025-03-12 18:19:25 +0100

    Implementacion de memoria en el modelo

--- Dedalus/pages/Chat.py
+++ Dedalus/pages/Chat.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from llm.image_processing import process_image
-from llm.chatbot import process_chat_message
+from core.image_processing import process_image
+from core.chatbot import process_chat_message
 
 st.set_page_config(page_title="ArkhamMed LLM", page_icon=":bat:")
 
@@ -16,7 +16,9 @@ st.markdown("### Adjuntar Archivos")
 archivos = st.file_uploader("Seleccionar archivos para adjuntar", accept_multiple_files=True)
 
 # Historial de conversación
+#Debería de guardarse al refrescar la página
 st.markdown("### Historial de conversación")
+#Itera sobre el historial de conversación y muestra los mensajes
 for mensaje in st.session_state.chat_history:
     with st.chat_message(mensaje["role"]):
         st.markdown(mensaje["content"])
@@ -25,11 +27,12 @@ for mensaje in st.session_state.chat_history:
 prompt = st.chat_input("Escribe tu mensaje...")
 
 if prompt:
+    #Esto es para poner el mensaje del usuario en el chat
     with st.chat_message("user"):
         st.markdown(prompt)
-
+    #Esto guarda el mensaje del usuario en el historial de conversación
     st.session_state.chat_history.append({"role": "user", "content": prompt})
-
+    
     # Analizar archivos si los hay
     file_context = ""
     if archivos:
@@ -37,8 +40,8 @@ if prompt:
             file_context = "\n".join([process_image(file) for file in archivos])
 
     # Generar respuesta del chatbot
-    respuesta_ia = process_chat_message(prompt, idioma, file_context)
-
+    respuesta_ia = process_chat_message(prompt, idioma, file_context, st.session_state.chat_history) #.append(st.session_state.chat_history)
+        
     with st.chat_message("assistant"):
         st.markdown(respuesta_ia)
 
