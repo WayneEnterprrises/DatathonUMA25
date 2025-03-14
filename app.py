7609de20b4c308d23b93dd73232dd5fd62b0090b
Anon2148 <privext@tutanota.com>
2025-03-14 04:20:07 +0100

    feat: fixed a typo in page path

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 pages = [
-    st.Page("pages/Home.py", title="Sobre Nosotros"),
+    st.Page("pages/Sobre_Nosotros.py", title="Sobre Nosotros"),
     st.Page("pages/Chat.py", title="Chat"),
     st.Page("pages/Contactanos.py", title="Contactanos")
 ]
